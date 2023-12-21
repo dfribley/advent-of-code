13670f4e56644a1edfe24357ef76cbf4a6179784
David Fribley <17037664+dfribley@users.noreply.github.com>
2023-12-21 13:34:54 -0500

    2020 - Day 7

--- 2020/Day7/Program.cs
+++ 2020/Day7/Program.cs
@@ -1,3 +1,5 @@
+using AoC.Shared.Strings;
+
 Console.WriteLine("AOC - Day 7\n\n");
 
 foreach (var inputFile in new[] { "sample.txt", "input.txt" })
@@ -9,10 +11,51 @@ foreach (var inputFile in new[] { "sample.txt", "input.txt" })
 
     Console.WriteLine($"[{inputFile}]\n");
 
-    var input = File.ReadAllLines(inputFile)
+    var bags = File.ReadAllLines(inputFile)
         .Where(line => !string.IsNullOrEmpty(line))
-        .ToList();
+        .Select(line =>
+        {
+            var parts = line.Split(" ", 3, StringSplitOptions.RemoveEmptyEntries);
+            var bag = string.Concat(parts.Take(2));
+            var contents = line[(line.IndexOf("contain") + 8)..]
+                .Split(",", StringSplitOptions.RemoveEmptyEntries)
+                .Where(p => !p.StartsWith("no"))
+                .Select(p =>
+                {
+                    var pParts = p.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                    return (amount: pParts[0].ToInt32(), bag: string.Concat(pParts.Skip(1).Take(2)));
+                })
+                .ToArray();
 
-    Console.WriteLine($"Part 1:");
-    Console.WriteLine($"Part 2:\n");
+            return (bag, contents);
+        })
+        .ToDictionary(b => b.bag, b => b.contents);
+
+    bool containsShinyBag(string bag)
+    {
+        foreach(var subBag in bags[bag])
+        {
+            if (subBag.bag == "shinygold" || containsShinyBag(subBag.bag))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    int countSubBags(string bag)
+    {
+        var subBags = 0;
+
+        foreach (var subBag in bags[bag])
+        {
+            subBags += subBag.amount * countSubBags(subBag.bag) + subBag.amount;
+        }
+
+        return subBags;
+    }
+
+    Console.WriteLine($"Part 1: {bags.Keys.Where(containsShinyBag).Count()}");
+    Console.WriteLine($"Part 2: {countSubBags("shinygold")}\n");
 }
