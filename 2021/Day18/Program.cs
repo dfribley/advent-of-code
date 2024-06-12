c725d43c5316bed8c676c119c29b99d5c97e21b8
David Fribley <17037664+dfribley@users.noreply.github.com>
2024-06-11 20:16:49 -0400

    Added 2021 - wip

---
+++ 2021/Day18/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("AOC - Day 18\n");
+
+foreach (var inputFile in new[] { "sample.txt", "input.txt" })
+{
+    Console.WriteLine($"[{inputFile}]\n");
+
+    var test = File.ReadAllLines(inputFile)
+        .Where(line => !string.IsNullOrEmpty(line))
+        .ToList();
+    
+    Console.WriteLine(test.Count);
+}
