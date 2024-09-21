d19dab1404b7894ccac00ce65f0052a38f96a777
David Fribley <17037664+dfribley@users.noreply.github.com>
2024-09-20 22:54:57 -0400

    Added 2020 - Day 11

---
+++ 2020/Day11/Program.cs
@@ -0,0 +1,71 @@
+using System.Drawing;
+using AoC.Shared.Grid;
+
+Console.WriteLine("AOC - Day 11\n\n");
+
+foreach (var inputFile in new[] { "sample.txt", "input.txt" })
+{
+    if (!File.Exists(inputFile))
+    {
+        continue;
+    }
+
+    Console.WriteLine($"[{inputFile}]\n");
+
+    var seats = File.ReadAllLines(inputFile)
+        .Where(line => !string.IsNullOrEmpty(line))
+        .ToGrid();
+
+    int SimulateSeating(bool part1)
+    {
+        var seatsCopy = seats.Clone();
+
+        while (true)
+        {
+            var changes = new List<(Point, char)>();
+
+            foreach (var seat in seatsCopy)
+            {
+                if (seat.value == '.')
+                {
+                    continue;
+                }
+                
+                var neighbors = part1
+                    ? seatsCopy.Neighbors(seat.coordinate)
+                    : seatsCopy.NeighborsInSight(seat.coordinate, c => c == '.');
+
+                if (seat.value == 'L')
+                {
+                    if (neighbors.All((n => n.value != '#')))
+                    {
+                        changes.Add((seat.coordinate, '#'));
+                    }
+                }
+                else
+                {
+                    if (neighbors.Count(n => n.value == '#') >= (part1 ? 4 : 5))
+                    {
+                        changes.Add((seat.coordinate, 'L'));
+                    }
+
+                }
+            }
+        
+            if (changes.Count == 0)
+            {
+                break;
+            }
+        
+            foreach (var (coordinate, value) in changes)
+            {
+                seatsCopy[coordinate] = value;
+            }
+        }
+
+        return seatsCopy.Count(seat => seat.value == '#');
+    }
+    
+    Console.WriteLine($"Part 1: {SimulateSeating(true)}");
+    Console.WriteLine($"Part 2: {SimulateSeating(false)}\n");
+}
