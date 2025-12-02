c64c1f5dcd888cd9f9d7e00ef9c98d75eec6df8c
David Fribley <17037664+dfribley@users.noreply.github.com>
2025-12-02 01:28:10 -0500

    2025 - Day 2

---
+++ 2025/Day2/Program.cs
@@ -0,0 +1,76 @@
+// Advent of Code challenge: https://adventofcode.com/2025/day/2
+using AoC.Shared.Ranges;
+
+Console.WriteLine("AoC - Day 2\n\n");
+
+foreach (var inputFile in new[] { "sample.txt", "input.txt" })
+{
+    if (!File.Exists(inputFile))
+    {
+        continue;
+    }
+
+    Console.WriteLine($"[{inputFile}]\n");
+
+    var ranges = File.ReadAllText(inputFile)
+        .Split(',')
+        .Select(Range64.Parse)
+        .ToList();
+
+    var part1 = ranges
+        .SelectMany(r => r
+            .Where(num =>
+            {   
+                var numStr = num.ToString();
+                return numStr.Length % 2 == 0 && IsInvalidIdString(numStr, numStr.Length / 2);
+            })
+            .ToList()
+        )
+        .Sum();
+
+    Console.WriteLine($"Part 1: {part1}");
+    
+    var part2 = ranges
+        .SelectMany(r => r
+            .Where(IsInvalidId)
+            .ToList()
+        )
+        .Sum();
+    
+    Console.WriteLine($"Part 2: {part2}\n");
+}
+
+return;
+
+static bool IsInvalidId(long num)
+{
+    var str = num.ToString();
+    var chunk = str.Length / 2;
+
+    while (chunk > 0)
+    {
+        if (str.Length % chunk == 0 && IsInvalidIdString(str, chunk))
+        {
+            return true;
+        }
+        
+        chunk--;
+    }
+
+    return false;
+}
+
+static bool IsInvalidIdString(string idAsString, int sequenceLength)
+{
+    var pattern = idAsString[..sequenceLength];
+
+    for (var i = 0; i < idAsString.Length; i += sequenceLength)
+    {
+        if (i + sequenceLength > idAsString.Length || idAsString.Substring(i, sequenceLength) != pattern)
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
--- Shared/Ranges/Range64.cs
+++ Shared/Ranges/Range64.cs
@@ -1,6 +1,9 @@
-﻿namespace AoC.Shared.Ranges;
+﻿using System.Collections;
+using AoC.Shared.Strings;
 
-public class Range64
+namespace AoC.Shared.Ranges;
+
+public class Range64 : IEnumerable<long>
 {
     private readonly long _start;
     private readonly long _end;
@@ -32,5 +35,27 @@ public class Range64
         return
             (_start <= range.Start && range.Start <= _end) ||
             (range.Start <= _start && _start <= range.End);
+    }
+    
+    public IEnumerator<long> GetEnumerator()
+    {
+        for (var i = _start; i <= _end; i++)
+            yield return i;
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+    public static Range64 Parse(string input)
+    {
+        if (!input.Contains('-'))
+        {
+            throw new ArgumentException("Range string must match format 'x-y'");
+        }
+        
+        var parts = input.Split('-');
+        var start = parts[0].ToInt64();
+        var end = parts[1].ToInt64();
+        
+        return new Range64(start, end - start + 1);
     }
 }
