d6b31dbaa1a71e05f25071363aa236159ce4f8be
David Fribley <17037664+dfribley@users.noreply.github.com>
2025-12-06 01:30:14 -0500

    2025 - Day 6

---
+++ 2025/Day6/Program.cs
@@ -0,0 +1,54 @@
+// Advent of Code challenge: https://adventofcode.com/2025/day/6
+
+using AoC.Shared.Enumerable;
+using AoC.Shared.Grid;
+
+Console.WriteLine("AoC - Day 6\n\n");
+
+foreach (var inputFile in new[] { "sample.txt", "input.txt" })
+{
+    if (!File.Exists(inputFile))
+    {
+        continue;
+    }
+
+    Console.WriteLine($"[{inputFile}]\n");
+
+    var input = File.ReadAllLines(inputFile).ToList();
+    var current = input.First().Length;
+    var problems = input
+        .Last()
+        .Select((c, i) => (c, i))
+        .Where(t => t.c != ' ')
+        .Reverse()
+        .Select(t =>
+        {
+            var grid = new Grid(input
+                .Take(input.Count - 1)
+                .Select(line => line.Substring(t.i, current - t.i))
+            );
+
+            current = t.i - 1;
+            return (op: t.c, grid);
+        })
+        .ToList();
+    
+    Console.WriteLine($"Part 1: {GrandTotal()}");
+
+    problems = problems
+        .Select(t => (t.op, grid: t.grid.Rotate90Prime()))
+        .ToList();
+    
+    Console.WriteLine($"Part 2: {GrandTotal()}\n");
+    
+    continue;
+
+    long GrandTotal()
+    {
+        return problems
+            .Select(t => t.op == '+' 
+                ? t.grid.rows.Select(long.Parse).Sum() 
+                : t.grid.rows.Select(long.Parse).Product())
+            .Sum();
+    }
+}
